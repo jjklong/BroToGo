b4b1777376ecf9f725a3e9e2f74cedc2e8df587a
Jordan Long <jordan.long222@gmail.com>
2016-06-21 16:13:58 -0400

    add beer and weather api connectivity with syncing longitude latitude coordinates of brewery location to autopopulate the current weater at that brewery

--- assets/js/brew.js
+++ assets/js/brew.js
@@ -13,13 +13,53 @@ $('.formInput').submit(function(e){
       url: 'https://api.brewerydb.com/v2/locations/?locality=' + city + '&key=905415c2a8c587c6f31b9eef1f71b4c0&format=json',
       success: function brewCity(user){
         var breweryResults = "";
+        var latitude = "";
+        var longitude = "";
+
+        latitude = user.data[0].latitude;
+        longitude = user.data[0].longitude;
+        console.log(latitude);
+        console.log(longitude);
+
         console.log(user);
         for (b=0;b<=5;b++){
           breweryResults = user.data[b].brewery.name;
           console.log(breweryResults);
           $('#chatbox').append('<li class="bot">' + breweryResults + '</li>');
         }
+
+        $.ajax({
+          url: 'https://api.forecast.io/forecast/77f5b9251d4b7d953c5e1f816e7f027b/' + latitude + ',' + longitude,
+          success: function weather(ll){
+            var temp = "";
+            var summary = "";
+            temp=ll.currently.temperature;
+            summary=ll.hourly.summary;
+            $('#tempF').html(Math.round(temp));
+            $('#tempIcon').css('display','initial');
+
+//detect SUN, RAIN, CLOUD, OR STORM IN THE AJAX RETURN FOR THE icon information from dark sky:
+// if, else statement to determine icon
+            var suni = ll.minutely.icon.match(/sun/g);
+            var cleari = ll.minutely.icon.match(/clear/g);
+            var raini = ll.minutely.icon.match(/rain/g);
+            var cloudi = ll.minutely.icon.match(/cloud/g);
+            var stormi = ll.minutely.icon.match(/storm/g);
+            $('#weatherIcon').removeClass("");
+            if (suni=='sun'||cleari=='clear'){
+              $('#weatherIcon').addClass('fa fa-sun-o');
+            } else if (raini!==null){
+              $('#weatherIcon').addClass('fa fa-tint');
+            } else if (cloudi!==null){
+              $('#weatherIcon').addClass('fa fa-cloud');
+            } else if (stormi!==null){
+              $('#weatherIcon').addClass('fa fa-bolt');
+            }
+          }
+        });
+
       }
     });
+
   }
 });
