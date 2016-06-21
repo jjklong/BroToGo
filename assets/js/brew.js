e7f2ef315f6c10590baec22fc0cff10cd3495680
Jordan Long <jordan.long222@gmail.com>
2016-06-21 11:50:34 -0400

    merge

--- assets/js/brew.js
+++ assets/js/brew.js
@@ -5,19 +5,21 @@ $('.formInput').submit(function(e){
   var beer = '@beer';
   console.log(request);
 
-  var city = request.replace(beer,"");
-  console.log(city);
+  if (request.startsWith(beer)===true){
+    var city = request.replace(beer,"");
+    console.log(city);
 
-  $.ajax({
-    url: 'https://api.brewerydb.com/v2/locations/?locality=' + city + '&key=905415c2a8c587c6f31b9eef1f71b4c0&format=json',
-    success: function brewCity(user){
-      var breweryResults = "";
-      console.log(user);
-      for (b=0;b<=5;b++){
-        breweryResults = user.data[b].brewery.name;
-        console.log(breweryResults);
-        $('#chatbox').append('<li class="bot">' + breweryResults + '</li>');
+    $.ajax({
+      url: 'https://api.brewerydb.com/v2/locations/?locality=' + city + '&key=905415c2a8c587c6f31b9eef1f71b4c0&format=json',
+      success: function brewCity(user){
+        var breweryResults = "";
+        console.log(user);
+        for (b=0;b<=5;b++){
+          breweryResults = user.data[b].brewery.name;
+          console.log(breweryResults);
+          $('#chatbox').append('<li class="bot">' + breweryResults + '</li>');
+        }
       }
-    }
-  });
+    });
+  }
 });
